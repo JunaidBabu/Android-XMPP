7c336452a705a8c3e216253b1e7fabbd53a893b4
Junaid Babu <junaidvayoli@gmail.com>
2014-03-30 17:15:55 +0530

    That's a working version of the gtalk in jabber level

---
+++ app/src/main/java/in/junaidbabu/androidxmpp/app/MainActivity.java
@@ -0,0 +1,120 @@
+package in.junaidbabu.androidxmpp.app;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.filter.MessageTypeFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.util.StringUtils;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private ArrayList<String> messages = new ArrayList();
+    private Handler handler = new Handler();
+    private xmppsettings settings;
+    private EditText recipient;
+    private EditText text;
+    private ListView list;
+    public static XMPPConnection connection;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recipient = (EditText) this.findViewById(R.id.recipient);
+        text = (EditText) this.findViewById(R.id.text);
+        list = (ListView) this.findViewById(R.id.messageList);
+        setListAdapter();
+
+        //Window for getting settings
+        settings = new xmppsettings(this);
+
+        //Listener for chat message
+        Button send = (Button) this.findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String to = recipient.getText().toString();
+                String text1 = text.getText().toString();
+
+                Message msg = new Message(to, Message.Type.chat);
+                msg.setBody(text1);
+                connection.sendPacket(msg);
+                messages.add(connection.getUser() + ":");
+                messages.add(text1);
+                setListAdapter();
+            }
+        });
+    }
+
+    //Called by settings when connection is established
+
+    public void setConnection (XMPPConnection connection) {
+        this.connection = connection;
+        if (connection != null) {
+            Log.i("Log main", "Phew, connection is not null");
+            //Packet listener to get messages sent to logged in user
+            PacketFilter filter = new MessageTypeFilter(Message.Type.chat);
+            connection.addPacketListener(new PacketListener() {
+                public void processPacket(Packet packet) {
+                    Message message = (Message) packet;
+                    if (message.getBody() != null) {
+                        String fromName = StringUtils.parseBareAddress(message.getFrom());
+                        messages.add(fromName + ":");
+                        messages.add(message.getBody());
+                        handler.post(new Runnable(){
+                            public void run() {
+                                setListAdapter();
+                            }
+                        });
+                    }
+                }
+            }, filter);
+        }
+    }
+
+    private void setListAdapter() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, R.layout.list, messages);
+        list.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                settings.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
---
+++ app/src/main/java/in/junaidbabu/androidxmpp/app/xmppsettings.java
@@ -0,0 +1,97 @@
+package in.junaidbabu.androidxmpp.app;
+
+
+import android.app.Dialog;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Presence;
+
+import java.io.File;
+//settings get input and then connection is established
+/**
+ * Created by neo on 30/3/14.
+ */
+
+public class xmppsettings extends Dialog implements android.view.View.OnClickListener  {
+    private MainActivity chatClient;
+
+    public xmppsettings(MainActivity chatClient){
+        super(chatClient);
+        this.chatClient = chatClient;
+    }
+
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.settings);
+        setTitle("Connection Settings");
+        Button ok = (Button) findViewById(R.id.ok);
+        ok.setOnClickListener(this);
+    }
+    ConnectionConfiguration connectionConfig;
+    XMPPConnection connection;
+    String host, port, service, username, password;
+    public void onClick(View v) {
+        host = getText(R.id.host);
+        port = getText(R.id.port);
+        service = getText(R.id.service);
+        username = getText(R.id.userid);
+        password = getText(R.id.password);
+        //Log.i("FYI", host+port+service+username+password);
+        // Create connection
+
+        connectionConfig = new ConnectionConfiguration(host, Integer.parseInt(port), service);
+        connectionConfig.setSASLAuthenticationEnabled(false);
+        connectionConfig.setTruststoreType("BKS");
+        connection = new XMPPConnection(connectionConfig);
+
+        new ConnectToXmpp().execute();
+
+    }
+    private class ConnectToXmpp extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                connection.connect();
+                Log.i("Log", "Connection established, yaay!");
+            } catch (XMPPException ex) {
+                Log.i("Log", "Unable to connect to the server");
+                chatClient.setConnection(null);
+            }
+            try {
+                connection.login(username, password);
+
+                // Set status to online / available
+                Presence presence = new Presence(Presence.Type.available);
+                connection.sendPacket(presence);
+                chatClient.setConnection(connection);
+                Log.i("Log", "Life is peace :D");
+            } catch (XMPPException ex) {
+                Log.i("Log", "Pain only");
+                ex.printStackTrace();
+                chatClient.setConnection(null);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i("OpnPostExe", "Here");
+            //MainActivity.connection = connection;
+            dismiss();
+        }
+
+    }
+    private String getText(int id) {
+        EditText widget = (EditText) this.findViewById(id);
+        return widget.getText().toString();
+    }
+
+}
